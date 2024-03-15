8cf8e1535ad0c8a5b2f2e16b316deb3e570eb2b7
tomroolfs <tomwillem04@gmail.com>
2024-03-15 16:08:01 +0100

    text adventure game afgemaakt
    Finished the text adventure game

--- text_game/lib/main.dart
+++ text_game/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'story_brain.dart';
 
 void main() {
   runApp(
@@ -9,9 +10,16 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
+StoryBrain storyBrain = StoryBrain();
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,17 +27,57 @@ class HomePage extends StatelessWidget {
         decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage("images/background.png"), fit: BoxFit.cover)),
-        child:const SafeArea(
+        child: SafeArea(
             child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Expanded(
+             Expanded(
                 child: Center(
-              child: Text(
-                'Hier komt de text game',
-                style: TextStyle(fontSize: 25, color: Colors.white),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  storyBrain.getStory(),
+                  style: const TextStyle(fontSize: 25, color: Colors.white),
+                ),
               ),
-            ))
+            )),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SizedBox(
+                  height: 75,
+                  child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          storyBrain.nextStory(1);
+                        });
+                      },
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.lightBlueAccent,
+                          foregroundColor: Colors.white,
+                          textStyle: const TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold)),
+                      child: Text(storyBrain.getChoice1()))),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Visibility(
+                visible: storyBrain.buttonShouldBeVisible(),
+                child: SizedBox(
+                    height: 75,
+                    child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            storyBrain.nextStory(2);
+                          });
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.lightBlueAccent,
+                            foregroundColor: Colors.white,
+                            textStyle: const TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold)),
+                        child: Text(storyBrain.getChoice2()))),
+              ),
+            ),
           ],
         )),
       ),
